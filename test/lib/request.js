c0cff5f77022495590132f1768ae7d9791330f33
zach balder <zmbalder@gmail.com>
2021-06-03 01:13:37 -0400

    make callbacks optional, add general request timeout

---
+++ test/lib/request.js
@@ -0,0 +1,25 @@
+const assert = require('assert')
+const fakeTimers = require('@sinonjs/fake-timers')
+const request = require('../../lib/request')
+
+describe.only('lib/request', () => {
+  beforeEach(() => {
+    this.clock = fakeTimers.install()
+  })
+
+  afterEach(() => {
+    this.clock.uninstall()
+  })
+
+  it('times out', async () => {
+    try {
+      await request('https://some.where', { method: 'POST' }, () => {
+        this.clock.tick(10e3)
+      })
+
+      assert.fail('Should reject')
+    } catch ({ message }) {
+      assert.strictEqual(message, 'POST request to "https://some.where/" timed out')
+    }
+  })
+})
